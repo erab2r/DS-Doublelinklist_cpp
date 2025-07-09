eaa31566b865e0a99a8d1e95a7f3d44e80613525
erab2r <shkera213@gmail.com>
2025-07-09 18:37:39 +0600

    sdfsf

--- input_doubly.cpp
+++ input_doubly.cpp
@@ -32,22 +32,21 @@ void insert_at_tail(Node* &head,Node* &tail,int val){
     tail->next = newNode;
     newNode->prev = tail;
     tail = newNode;
-
-}
+}   
 int main(){
-    Node* head = new Node(100);
-    Node* a = new Node(300);
-    Node* b = new Node(400);
-    Node* tail = new Node(500);
-
-    head->next = a;
-    a->prev = head;
-    a->next = b;
-    b->prev = a;
-    b->next = tail;
-    tail->prev = b;
-    insert_at_tail(head,tail,800);
+    Node* head = NULL ;
+    Node* tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if(val ==-1){
+            break;
+        }
+        insert_at_tail(head,tail,val);
+    }
+    
     print_forward(head);
 
     return 0;
-}
+}    
